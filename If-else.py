8f43c024606a2b525d125e867e363c4a43806676
Putrevu Rajasekhara Rao HYD EDETY45 <rajasekhararao.putrevu@zf.com>
2025-05-20 17:38:37 +0530

    rajasekhar added a message

--- If-else.py
+++ If-else.py
@@ -7,9 +7,16 @@ elif age<=12:
 elif age <= 19:
     print("Teen")
 elif age <= 59:
+
     print("Adult")
+elif age >= 666:
+    print("Dengey")
 else:
     print("Senior")
+
     
     
-    
+#this is from clone i have made changes
+#do you know?#
+
+#hi how r u?
